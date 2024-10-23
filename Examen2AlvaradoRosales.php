2f34d4181ae64c08f77b74688fde80b4e23f00cc
mildrethyeseniaalvaradorosales <mildrethalvaradorosales1@gmail.com>
2024-10-23 09:56:45 -0600

    Add files via upload
    Examen de la segunda unidad

--- Examen2AlvaradoRosales.php
+++ Examen2AlvaradoRosales.php
@@ -1,23 +1,45 @@
 <?php
 /* CBTIS 89
 Examen2AlvaradoRosales.php
-//Programa que almacena el precio de 5 articulos
+Programa que almacena el precio de 5 articulos en arraglos calcula el IVA ,el subtotal ,calcula un descuento y los almacena en un nuevos arreglos y imprime los arreglos.
 Mildreth Yesenia Alvarado Rosales
 3°A Programación Matutino */
 $arrayPrecios=array(500,400,200,700,100);
-$arrayIVA=array(0.16);
+$arrayIVA=array();
 $arraySubtotal=array();
-$arrayDescuentos=array(0.10);
+$arrayDescuentos=array();
 $arrayTotal=array();
+
 $longitud = count($arrayPrecios); 
 for ($i = 0; $i < $longitud; $i++) {
-$arrayPrecios[$i]=$arrayPrecios[$i]*$arrayIVA[$i];
-$arraySubtotal=$arrayPrecios[$i]-$arrayIVA[$i];
-$arrayDescuentos=$arraySubtotal[$i]*$arrayDescuentos[$i];}
-echo "Arreglo Precios:".count($arrayPrecios);
-echo "Arreglo IVA:".count($arrayIVA);
-echo "Arreglo Subtotal:".count($arraySubtotal);
-echo "Arreglo Descuentos:".count($arrayDescuentos);
-echo "Arreglo Precios:".count($arrayPrecios);
-echo "Arreglo Total:".count($arrayTotal);
+    $arrayIVA[$i] = $arrayPrecios[$i] * 0.16;
+    $arraySubtotal[$i] = $arrayPrecios[$i] + $arrayIVA[$i];
+    $arrayDescuentos[$i] =$arraySubtotal[$i] *0.10;
+    $arrayTotal[$i]=$arraySubtotal[$i]-$arrayDescuentos[$i];
+}
+echo "Arreglo Precios <br>";
+for ($i = 0; $i < $longitud; $i++) {
+    echo $arrayPrecios[$i];
+       echo "<br>","<br>";
+}
+echo "Arreglo IVA<br>";
+for ($i = 0; $i < $longitud; $i++) {
+    echo   $arrayIVA[$i] ;
+   echo "<br>","<br>";
+}
+echo "Arreglo Subtotal <br>";
+for ($i = 0; $i < $longitud; $i++) {
+    echo  $arraySubtotal[$i];
+    echo "<br>","<br>";
+}
+echo "Arreglo Descuentos <br>";
+for ($i = 0; $i < $longitud; $i++) {
+    echo $arrayDescuentos[$i];
+       echo "<br>","<br>";
+}
+echo "Arreglo Total <br>";
+for ($i = 0; $i < $longitud; $i++) {
+    echo  $arrayTotal[$i];
+       echo "<br>","<br>";
+}
 ?>
